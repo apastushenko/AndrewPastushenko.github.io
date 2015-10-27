67b2bad81f6afff1e57d62f141a3f3f1d93c6345
apastushenko <andrew.pastushenko@gmail.com>
2015-10-27 19:18:03 +0200

    lesson 7 task 3

--- GoIT/javascript/home/lesson7/codewars3.js
+++ GoIT/javascript/home/lesson7/codewars3.js
@@ -4,3 +4,11 @@
 var make_lazy = function (fn) {
     return fn.bind.apply(fn, arguments);
 };
+
+//var make_lazy = function (fn) {
+//    return fn.bind.apply(fn, arguments);
+//};
+//function add(a,b){
+//    return a+b;
+//}
+//console.log( make_lazy(add, 2 ,3)())
