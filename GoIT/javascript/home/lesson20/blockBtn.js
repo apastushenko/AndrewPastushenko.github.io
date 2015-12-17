bd176cd340ad3c57e286a1983fd8f3b2012da78d
apastushenko <andrew.pastushenko@gmail.com>
2015-12-17 17:52:16 +0200

    hw

---
+++ GoIT/javascript/home/lesson20/blockBtn.js
@@ -0,0 +1,15 @@
+var inputField = document.body.querySelector('.task-form__text');
+
+var btn = document.body.querySelector('.btn-primary');
+
+inputField.addEventListener('input', blockBtn);
+
+function blockBtn () {
+
+    if (!inputField.value) {
+        btn.disabled = true;
+    } else {
+        btn.disabled = false;
+    }
+
+}
